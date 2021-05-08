366931de53611263ff41ae9901ce559ff9142d6a
adamloza8 <adamloza8@gmail.com>
2021-05-08 13:34:24 +0200

    Stage 21b

--- API/Startup.cs
+++ API/Startup.cs
@@ -59,6 +59,8 @@ namespace API
         // app.UseHttpsRedirection();
 
         app.UseRouting();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
         app.UseCors("CorsPolicy");
 
         app.UseAuthentication();
@@ -68,6 +70,7 @@ namespace API
         {
             endpoints.MapControllers();
             endpoints.MapHub<ChatHub>("/chat");
+            endpoints.MapFallbackToController("Index", "Fallback");
         });
     }
 }
