d0769acd5513e0a6eab215ecef584158f5c0e786
adamloza8 <adamloza8@gmail.com>
2021-05-09 18:30:33 +0200

    refresh token

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -27,7 +27,12 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");    
+                    policy
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithExposedHeaders("WWW-Authenticate", "Pagination")
+                    .WithOrigins("http://localhost:3000");    
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
